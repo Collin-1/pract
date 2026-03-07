7def6076461a0ab05be813b6580d164c1d08b848
Collin Makwala <collinmakwala@gmail.com>
2026-03-07 12:37:12 +0200

    add question 2

--- prep_20.py
+++ prep_20.py
@@ -3,3 +3,16 @@
 def reverse_string(s):
     return s[::-1]
 
+# Question 2
+# Find duplicates in a list.
+def find_duplicates(nums):
+    seen = set()
+    duplicates = set()
+
+    for n in nums:
+        if n in seen:
+            duplicates.add(n)
+        else:
+            seen.add(n)
+
+    return list(duplicates)
