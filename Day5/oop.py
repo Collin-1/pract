8f4f89a2db3c31cee44a9b14267830a4e6e01c21
Collin Makwala <collinmakwala@gmail.com>
2026-03-08 21:07:51 +0200

    add shopping cart class

--- Day5/oop.py
+++ Day5/oop.py
@@ -19,3 +19,30 @@ class CartItem:
 # composition is a design principle where a class contains objects of other classes as its attributes
 # to build complex functionality, modeling a "has-a" relationship.
 
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_product(self, product, quantity):
+        item = CartItem(product, quantity)
+        self.items.append(item)
+
+    def total_cost(self):
+        total = 0
+        for item in self.items:
+            total =+ item.total_price()
+        return total
+    
+    def show_cart(self):
+        for item in self.items:
+            print(f"{item.product.name} x{item.quantity}")
+
+cart = ShoppingCart()
+
+laptop = Product("Laptop", 15000)
+mouse = Product("Mouse", 300)
+
+cart.add_product(laptop, 1)
+cart.add_product(mouse, 2)
+
+cart.show_cart()
