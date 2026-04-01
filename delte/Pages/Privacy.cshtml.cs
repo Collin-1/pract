829c50eee7aaffabe9c4adf9a08ec8e046eeb59b
Collin Makwala <collinmakwala@gmail.com>
2026-04-01 11:58:14 +0200

    add delte pages

---
+++ delte/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace delte.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
